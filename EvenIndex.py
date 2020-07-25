0114a9e01e787fba5d1a8c5bb19723854dc507b1
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-25 16:46:35 +0530

    Code Updated

--- EvenIndex.py
+++ EvenIndex.py
@@ -1,1 +1,12 @@
-
+#Priting the Value at Even Places 
+T = (1,2,3,4,5)
+print('Original Tuple elements are :',end=' ')
+print(T)
+l=[]
+for i in T:
+    #for elemnts having Remainder 0.
+    if T.index(i)%2==0:
+        l.append(i)
+T2 = tuple(l)
+print('Values at Even index are',end=' ')
+print(T2)
