96e39a2fc78bc3f951bfaa7901ab173e32576448
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-25 16:56:39 +0530

    Code Improved

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,1 +1,17 @@
+string1 = str(input("Enter a string to reverse :"))
 
+
+def reverse_string(string):
+
+    for i in string:
+        string = i + string
+    return string
+
+
+print("The original string : ", string1)
+
+print("The reverse of the string: ", reverse_string(string1))
+
+string2 = "my name is sanket godbole"
+
+print(string2[::-1])
