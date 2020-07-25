2a07fcd2cdc4c3f8c1479189feba1496ac0f8f8b
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-25 16:32:27 +0530

    Update CeltoFer.py

--- CeltoFer.py
+++ CeltoFer.py
@@ -1,1 +1,8 @@
-
+#Conversion of Celcius to Fahrenheit Degrees
+print('Enter 5 Celcius Values:')
+vals = [int(input()) for x in range(5)]
+Fer =[]
+for i in vals:
+    Fer.append(i*9/5+32)
+print('Fehrenheit values are')
+print(Fer)
