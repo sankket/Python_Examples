7a9b8edefee60d9477f8009d89cc5b59112442ff
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-25 16:39:55 +0530

    Code Improved

--- Fibinacci.py
+++ Fibinacci.py
@@ -1,1 +1,15 @@
+#Fibonnaci Series
+a = int(input("Enter First Number:"))
+b = int(input("Enter Second number"))
+n = int(input("How many numbers you want in Series"))
 
+print(a,b)
+
+while(n>=0):
+    c =a + b
+    a=b
+    b=c
+    print(c)
+    n=n-1
+
+# 1 2 3 5 8 13 21 .....
