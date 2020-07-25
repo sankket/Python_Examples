aaa45565e3c52aa6011d8a88076f3a376f9fab05
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-07-25 16:58:43 +0530

    Code Updated

--- Inheritance_Animal.py
+++ Inheritance_Animal.py
@@ -1,1 +1,20 @@
+class Animal:
+    def eat(self):
+        print('Eating')
+class Dog(Animal):
+    def bark(self):
+        print('Dog is barking')
+class Cat(Dog): 
+    def meowing(self):
+        print('cat is meowing')
+class Janwar(Cat):
+    def sleep(self):
+        print('Janwar is sleeping ')
+
+janwar = Janwar()
+janwar.sleep()
+janwar.meowing()
+janwar.bark()
+janwar.eat()
+
 
