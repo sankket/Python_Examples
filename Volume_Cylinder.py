f18282bf601b2c756417ba86c1e8e99aae3a9e20
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-04 14:14:29 +0530

    Code Updated.

--- Volume_Cylinder.py
+++ Volume_Cylinder.py
@@ -1,1 +1,13 @@
-
+#Find the Volume of the Cylinder.
+class Volume:
+    def __init__(self):
+        self.a =1
+        self.f=1
+        self.c=1
+    def volume(self,l,b,h):
+        self.l=l
+        self.b=b
+        self.h=h
+        print('Volume of Box is ',l*b*h)
+vol = Volume()
+vol.volume(4,6,8)
