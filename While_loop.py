d9c6239af9c1157d49a8acdcc2e962957281dffb
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-06 17:56:21 +0530

    Update While_loop.py

--- While_loop.py
+++ While_loop.py
@@ -6,3 +6,6 @@ while num < 10:
     print ('number is currently: ',num)
     print (' number is still less than 10, adding 1 to num')
      num+=1
+        
+
+#With the while loop we can execute a set of statements as long as a condition is true.
