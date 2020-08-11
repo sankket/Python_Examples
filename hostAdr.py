c12ee8ed1592095239d85e8abb481751d17dd35e
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-11 13:25:35 +0530

    Code Updated.

--- hostAdr.py
+++ hostAdr.py
@@ -1,1 +1,7 @@
-
+# Getting the host address program in python.
+import socket
+#gets host address By follwing Method
+add1=socket.gethostbyaddr("216.58.203.132")
+add2=socket.gethostbyaddr("106.10.250.10")
+print("The Address is Of:",add1)
+print("The Address is Of:",add2)
