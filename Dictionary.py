8c40aef5d09f3d5319cdfe8e6213b2174cb5c3c0
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-12 14:54:57 +0530

    Code Improved.

--- Dictionary.py
+++ Dictionary.py
@@ -1,1 +1,16 @@
-
+#Dictionary is mutable.
+person=dict(Name="Harish",Age=23,Salary=90000)
+print(person)
+print(person["Name"])
+print("update name")
+person["Name"]="Hari marllapale"
+print(person["Name"])
+print("add Insurance key")
+person["Insurance"]="Yes"
+print(person)
+print("delete age")
+del person["Age"]
+print(person)
+#print Salary of Hari
+print("The salary of Hari is:")
+print(person["Salary"])
