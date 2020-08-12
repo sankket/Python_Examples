9b6bcd3144329cd45721cc5a997b28d4101c922b
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-12 14:51:33 +0530

    Code Updated

--- Read_File.py
+++ Read_File.py
@@ -1,1 +1,11 @@
-
+#Reading Data from the File.
+def main():
+    try:
+        ReadFile=open("x.txt","r")
+        for line in ReadFile:
+            print(line)
+        ReadFile.close()
+    except IOError:
+        print("File not found")
+    print("app is done")
+if __name__ == '__main__':main()
