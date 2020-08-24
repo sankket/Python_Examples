ce0df181582d2974dcea018d4d704d6500134edb
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-24 16:39:19 +0530

    Code Updated.

--- Two_sum.py
+++ Two_sum.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        table = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement not in table:
+                table[nums[i]] = i
+            else:
+                return [table[complement], i]
