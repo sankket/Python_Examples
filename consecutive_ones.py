032209a17b06dca6710df61b8e1b2500b0fe9410
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-24 16:41:01 +0530

    Code Updated.

--- consecutive_ones.py
+++ consecutive_ones.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        count_global = 0
+        count_cur = 0
+        for i in nums:
+            if i == 1:
+                count_cur += 1
+            else:
+                if count_cur > count_global:
+                    count_global = count_cur
+                count_cur = 0
+        return max(count_global, count_cur)
