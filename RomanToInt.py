fdda82df6a35c4d84058c1a466844824ae0364dd
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-08-24 16:36:08 +0530

    Code Updated.

--- RomanToInt.py
+++ RomanToInt.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sums=0
+        count=None
+        d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        for i in range(len(s)-1):
+            if d[s[i]]>=d[s[i+1]]:
+                sums+=d[s[i]]
+            else:
+                sums+= -d[s[i]]
+        sums+=d[s[len(s)-1]]
+        return sums
