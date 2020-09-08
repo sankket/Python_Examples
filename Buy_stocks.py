c92c9aeb58724f2f09ec028220299a25f79e024d
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-09-08 15:35:24 +0530

    Code updated.

--- Buy_stocks.py
+++ Buy_stocks.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+        
+        maxprofit = 0
+        
+        for i in range(len(prices)-1):
+            if prices[i+1]> prices[i]:
+                
+                maxprofit += prices[i+1] -  prices[i]
+            
+        return maxprofit
