c9394dd4a9fd35ac779023f0c7f030d49fadcc80
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-09-16 16:03:20 +0530

    Code Updated.

--- House_Robber.py
+++ House_Robber.py
@@ -1,1 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums) if nums else 0
+        i_minus_3 = 0
+        i_minus_2 = nums[0]
+        i_minus_1 = nums[1]
+        
+        for num in nums[2:]:
+            current = max(i_minus_1,i_minus_2 + num, i_minus_3 + num)
+            i_minus_3 = i_minus_2
+            i_minus_2 = i_minus_1
+            i_minus_1 = current
+        
+        return i_minus_1
 
