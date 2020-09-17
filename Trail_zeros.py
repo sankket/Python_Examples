c238d3b7cf8cda835945c4faa72cede324400fea
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-09-17 19:39:41 +0530

    Code Improved.

--- Trail_zeros.py
+++ Trail_zeros.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def trailingZeroes(self, num: int) -> int:
+        counter = 0
+        while num >= 5:
+            num = num // 5
+            counter += num
+        return counter
+            
+        
