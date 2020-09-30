923e019f5b3b92385abdf44cd756a66cbdbe63aa
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-09-30 14:56:53 +0530

    Code Updated.

--- Domain_names.py
+++ Domain_names.py
@@ -1,1 +1,5 @@
-
+# Getting IP address using socket library in python.
+import socket
+add1=socket.gethostbyname("www.google.com")
+add2=socket.gethostbyname("www.yahoo.com")
+print(add1,"\t", add2)
