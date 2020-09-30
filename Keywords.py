22e19a05c30c12d770f094111072f8794684803d
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2020-09-30 14:52:08 +0530

    Code Updated.

--- Keywords.py
+++ Keywords.py
@@ -1,1 +1,6 @@
+#keywords in python.
+def keyword_and_positional(name='Sherlock',message='Nice to meet you'):
+    print('Hello',name,message)
 
+def area(width=1,height=1):
+    return width*height
