6c062a9763b24de60fe43ee8fa558b40a07ab075
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-10-07 16:54:36 +0530

    Code Improved.

--- Binary_addition.py
+++ Binary_addition.py
@@ -1,3 +1,6 @@
+# binary addition of the two numbers.
+# The slicing is used.
+
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         a = int(a,2)
@@ -5,6 +8,5 @@ class Solution:
         c = a + b
         return bin(c)[1:]
     
-# binary addition of the two numbers.
-# The slicing is used.
+
         
