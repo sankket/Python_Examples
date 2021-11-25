f22d48ce8148c868d26ea812dc7580c0bbde92ec
Sanket Godbole <35602775+sankket@users.noreply.github.com>
2021-11-25 11:46:13 +0530

    Code Improved.

--- get_dns.py
+++ get_dns.py
@@ -1,5 +1,5 @@
 # Importing the dns address of the website.
 import socket
-add1=socket.gethostbyname("www.google.com")
-add2=socket.gethostbyname("www.yahoo.com")
+add1=socket.gethostbyname("www.infosys.com")
+add2=socket.gethostbyname("www.microsoft.com")
 print(add1,"\t", add2)
